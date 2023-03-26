e8e20c64bc269317491f617e36e7e9a92f5e3f70
unknown <matthewajustice@gmail.com>
2023-03-26 12:45:01 -0400

    fixed eas property in app.config.js

--- app.config.js
+++ app.config.js
@@ -43,7 +43,9 @@ export default {
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
       appId: process.env.APP_ID,
-      easProjectId: process.env.EAS_PROJECT_ID,
+      eas: {
+        projectId: process.env.EAS_PROJECT_ID,
+      },
     },
   },
 };
